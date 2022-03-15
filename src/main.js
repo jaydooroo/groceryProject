cd134d6b6e24be440332801da37747d4a87687a8
Jeyheon Lee <jehyeonlee27@gmail.com>
2022-03-15 15:09:45 -0600

    first commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import mock from "./mock-data.js";
 
-Vue.config.productionTip = false
+let data = {
+  products: mock,
+  cart: [],
+};
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  data,
+  render: (h) => h(App),
+}).$mount("#app");
